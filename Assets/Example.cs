e11998f04d9355b70074d6f4e822b39c83b30c0e
Lukas <lukas.almgren03@gmail.com>
2023-05-04 15:41:33 +0200

    Translation funkar, var glad :+1:

--- Assets/Example.cs
+++ Assets/Example.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using Vectors;
@@ -12,10 +13,26 @@ public class Example : MonoBehaviour {
     [NonSerialized] 
     private VectorRenderer vectors;
 
-    public Vector3 Target = Vector3.forward;
+    public bool ShowPosition = true;
+    public bool ShowScale = true;
+    public bool ShowRotation = true;
+
     [Range(0, 1)] public float Time = 0.0f;
 
-    internal Matrix4x4 A;
+    [SerializeField, HideInInspector] internal Matrix4x4 A;
+    [SerializeField, HideInInspector] internal Matrix4x4 B;
+    [SerializeField, HideInInspector] internal Matrix4x4 C;
+
+    [NonSerialized] public Vector4 vector0 = new Vector4(0,0,0,1);
+    [NonSerialized] public Vector4 vectorA = new Vector4(1,0,0,1);
+    [NonSerialized] public Vector4 vectorB = new Vector4(0,1,0,1);
+    [NonSerialized] public Vector4 vectorC = new Vector4(0,0,1,1);
+    [NonSerialized] public Vector4 vectorAB = new Vector4(1,1,0,1);
+    [NonSerialized] public Vector4 vectorAC = new Vector4(1,0,1,1);
+    [NonSerialized] public Vector4 vectorBC = new Vector4(0,1,1,1);
+    [NonSerialized] public Vector4 vectorABC = new Vector4(1,1,1,1);
+    
+    
     
     void OnEnable() {
         vectors = GetComponent<VectorRenderer>();
@@ -23,10 +40,32 @@ public class Example : MonoBehaviour {
 
     void Update()
     {
-        var aPos = new Vector3(A.m03, A.m13, A.m23);
-        var pos = (1.0f - Time) * aPos + Time * Target;
+        Vector4 posInter = (1.0f - Time) * Math.getPositionFromMatrix(A) + Time * Math.getPositionFromMatrix(B);
+        
+        if(ShowPosition)
+            C = Math.setPositionInMatrix(C, posInter);
+        
+
         using (vectors.Begin()) {
-            vectors.Draw(pos, pos + transform.up, Color.green);
+            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorA), Color.red);
+            vectors.Draw(CalculatePos(vectorC), CalculatePos(vectorAC), Color.red);
+            vectors.Draw(CalculatePos(vectorB), CalculatePos(vectorAB), Color.red);
+            vectors.Draw(CalculatePos(vectorBC), CalculatePos(vectorABC), Color.red);
+            
+            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorC), Color.blue);
+            vectors.Draw(CalculatePos(vectorA), CalculatePos(vectorAC), Color.blue);
+            vectors.Draw(CalculatePos(vectorB), CalculatePos(vectorBC), Color.blue);
+            vectors.Draw(CalculatePos(vectorAB), CalculatePos(vectorABC), Color.blue);
+            
+            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorB), Color.green);
+            vectors.Draw(CalculatePos(vectorA), CalculatePos(vectorAB), Color.green);
+            vectors.Draw(CalculatePos(vectorC), CalculatePos(vectorBC), Color.green);
+            vectors.Draw(CalculatePos(vectorAC), CalculatePos(vectorABC), Color.green);
+        }
+
+        Vector4 CalculatePos(Vector4 vec)
+        {
+            return (vec + Math.getPositionFromMatrix(C));
         }
     }
 }
@@ -40,12 +79,18 @@ public class DemoEditor : Editor
         if (!demo) return;
 
         EditorGUI.BeginChangeCheck();
-        var newTarget = Handles.PositionHandle(demo.Target, demo.transform.rotation);
+        
+        var aPos = new Vector3(demo.A.m03, demo.A.m13, demo.A.m23);
+        var newTarget = Handles.PositionHandle(aPos, demo.transform.rotation);
 
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(demo, "Moved target");
-            demo.Target = newTarget;
+            var copy = demo.A;
+            copy.m03 = newTarget.x;
+            copy.m13 = newTarget.y;
+            copy.m23 = newTarget.z;
+            demo.A = copy;
             EditorUtility.SetDirty(demo);
         }
     }
@@ -63,13 +108,63 @@ public class DemoEditor : Editor
         EditorGUILayout.PrefixLabel("Matrix A");
         EditorGUILayout.BeginVertical();
 
-        var result = Matrix4x4.identity;
+        var resultA = Matrix4x4.identity;
         for (var i = 0; i < 4; i++)
         {
             EditorGUILayout.BeginHorizontal();
             for (var j = 0; j < 4; j++)
             {
-                result[i, j] = EditorGUILayout.FloatField(demo.A[i, j]);
+                resultA[i, j] = EditorGUILayout.FloatField(demo.A[i, j]);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        
+        //Radmellanrum
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        
+        
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Matrix B");
+        EditorGUILayout.BeginVertical();
+
+        var resultB = Matrix4x4.identity;
+        for (var i = 0; i < 4; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (var j = 0; j < 4; j++)
+            {
+                resultB[i, j] = EditorGUILayout.FloatField(demo.B[i, j]);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        
+        //Radmellanrum
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        
+        
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Matrix C");
+        EditorGUILayout.BeginVertical();
+
+        var resultC = Matrix4x4.identity;
+        for (var i = 0; i < 4; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (var j = 0; j < 4; j++)
+            {
+                resultC[i, j] = EditorGUILayout.FloatField(demo.C[i, j]);
             }
             EditorGUILayout.EndHorizontal();
         }
@@ -80,7 +175,9 @@ public class DemoEditor : Editor
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(demo, "Change matrix");
-            demo.A = result;
+            demo.A = resultA;
+            demo.B = resultB;
+
             EditorUtility.SetDirty(demo);
         }
     }
--- Assets/Math.cs
+++ Assets/Math.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public static class Math
 {
@@ -14,5 +15,18 @@ public static class Math
         return new Vector3(vec1.y * vec2.z - vec1.z * vec2.y,
             vec1.z * vec2.x - vec1.x * vec2.z,
             vec1.x * vec2.y - vec1.y * vec2.x);
+    }
+
+    public static Vector4 getPositionFromMatrix(Matrix4x4 mat)
+    {
+        return new Vector4(mat.m03, mat.m13, mat.m23, 0);
+    }
+
+    public static Matrix4x4 setPositionInMatrix(Matrix4x4 mat, Vector4 vec)
+    {
+        mat.m03 = vec.x;
+        mat.m13 = vec.y;
+        mat.m23 = vec.z;
+        return mat;
     }
 }
