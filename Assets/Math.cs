1932acd99bae556427bb07c40bd08e2c8daf22a5
Lukas <lukas.almgren03@gmail.com>
2023-05-05 19:54:33 +0200

    Gud hjälpe mig
    -C matrisen actar upp, blir inte samma som B
    -kolla med varför omvänd q2.w i CalculateRotationMatrix (Crossproduct < 0, chatgpt)
    -Gör rent i koden

--- Assets/Math.cs
+++ Assets/Math.cs
@@ -52,9 +52,9 @@ public static class Math
         var y = q.y;
         var z = q.z;
         var w = q.w;
-        var c1 = new Vector4(1-2*y*y-2*z*z,2*x*y+2*w*z,2*x*z-2*w*y,0);
-        var c2 = new Vector4(2*x*y-2*w*z,1-2*x*x-2*z*z,2*y*z+2*w*x, 0);
-        var c3 = new Vector4(2*x*z+2*w*y,2*y*z-2*w*x,1-2*x*x-2*y*y,0);
+        var c1 = new Vector4(1 - 2*y*y - 2*z*z,2*x*y + 2*w*z,2*x*z - 2*w*y,0);
+        var c2 = new Vector4(2*x*y - 2*w*z,1 - 2*x*x - 2*z*z,2*y*z + 2*w*x, 0);
+        var c3 = new Vector4(2*x*z + 2*w*y,2*y*z - 2*w*x,1 - 2*x*x - 2*y*y,0);
         var c4 = new Vector4(0,0,0,1);
 
         return new Matrix4x4(c1,c2,c3,c4);
