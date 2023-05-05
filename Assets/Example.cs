bdd7c374be7c455de2cff2f751e1e6950e22b575
Lukas <lukas.almgren03@gmail.com>
2023-05-05 19:09:38 +0200

    Fuck you gud :point_up_2:

--- Assets/Example.cs
+++ Assets/Example.cs
@@ -8,8 +8,6 @@ using Vectors;
 [RequireComponent(typeof(VectorRenderer))]
 public class Example : MonoBehaviour {
     
-    //Timestamp video Emil: 1:05:50
-    
     [NonSerialized] 
     private VectorRenderer vectors;
 
@@ -24,14 +22,14 @@ public class Example : MonoBehaviour {
     [SerializeField, HideInInspector] internal Matrix4x4 B;
     [SerializeField, HideInInspector] internal Matrix4x4 C = Matrix4x4.identity;
 
-    [NonSerialized] public Vector4 vector0 = new Vector4(0,0,0,1);
-    [NonSerialized] public Vector4 vectorA = new Vector4(1,0,0,1);
-    [NonSerialized] public Vector4 vectorB = new Vector4(0,1,0,1);
-    [NonSerialized] public Vector4 vectorC = new Vector4(0,0,1,1);
-    [NonSerialized] public Vector4 vectorAB = new Vector4(1,1,0,1);
-    [NonSerialized] public Vector4 vectorAC = new Vector4(1,0,1,1);
-    [NonSerialized] public Vector4 vectorBC = new Vector4(0,1,1,1);
-    [NonSerialized] public Vector4 vectorABC = new Vector4(1,1,1,1);
+    [NonSerialized] public Vector4 vector0 = new Vector4(-0.5f,-0.5f,-0.5f,1);
+    [NonSerialized] public Vector4 vectorA = new Vector4(0.5f,-0.5f,-0.5f,1);
+    [NonSerialized] public Vector4 vectorB = new Vector4(-0.5f,0.5f,-0.5f,1);
+    [NonSerialized] public Vector4 vectorC = new Vector4(-0.5f,-0.5f,0.5f,1);
+    [NonSerialized] public Vector4 vectorAB = new Vector4(0.5f,0.5f,-0.5f,1);
+    [NonSerialized] public Vector4 vectorAC = new Vector4(0.5f,-0.5f,0.5f,1);
+    [NonSerialized] public Vector4 vectorBC = new Vector4(-0.5f,0.5f,0.5f,1);
+    [NonSerialized] public Vector4 vectorABC = new Vector4(0.5f,0.5f,0.5f,1);
 
     public Quaternion aRotation = Quaternion.identity;
     public Quaternion bRotation = Quaternion.identity;
@@ -50,9 +48,11 @@ public class Example : MonoBehaviour {
         //Quaternion lerpedRotation = Quaternion.Lerp(aRotation, bRotation, Time);
         
         Vector4 posInter = (1.0f - Time) * Math.getColumnFromMatrix(A, 3) + Time * Math.getColumnFromMatrix(B, 3);
-        if(ShowTranslation)
-            CTranslate = Matrix4x4.identity;
+        if (ShowTranslation)
+        {
             CTranslate = Math.setColumnInMatrix(Matrix4x4.identity, posInter, 3);
+        }
+            
             
         if (ShowScale)
         {
@@ -83,21 +83,38 @@ public class Example : MonoBehaviour {
 
         if (ShowRotation)
         {
-            CRotate = Matrix4x4.identity;
-
+            /*
             Quaternion lerpedRotation = Rotationweee(Time);
 
 
+            Quaternion quaternionA = weeewooo(A, Matrix4x4.identity);
+            
+            Quaternion quaternionB = weeewooo(B, Matrix4x4.identity);
 
+            Quaternion invertedQuaternionA = new Quaternion(quaternionA.x, quaternionA.y, quaternionA.z, quaternionA.w);
+
+            Quaternion CQuaternion1 = invertedQuaternionA;
+            
+            Quaternion CQuaternion3 = quaternionB * invertedQuaternionA * quaternionA;
+
+            lerpedRotation = quaternionA; //GetInterpolatedQuat(quaternionA, quaternionB);
+            
+            Debug.Log($"A:{quaternionA}" +
+                      $"B:{quaternionB}");
+            
             //Rotationen, fast fuskvärde
-            /*for (int i = 0; i < 3; i++)
+            /*
+            for (int i = 0; i < 3; i++)
             {
                 CRotate = Math.setColumnInMatrix(CRotate,
                     QuatMultVector3(lerpedRotation, Math.getColumnFromMatrix(CRotate, i)), i);
-            }*/
-            
-            CRotate = Matrix4x4.identity;
+            }
+            */
 
+
+            CRotate = Math.CalculateRotationMatrix(A, B, Time);
+
+            /*
             Vector3 temp;
 
             temp = QuatMultVector3(lerpedRotation, new Vector3(1, 0, 0));
@@ -114,32 +131,43 @@ public class Example : MonoBehaviour {
             CRotate.m02 = temp.x;
             CRotate.m12 = temp.y;
             CRotate.m22 = temp.z;
+            */
         }
 
         //Ordningen som är smart
         C =  CTranslate * CRotate * CScale;
+        
 
 
         using (vectors.Begin()) {
-            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorA), Color.red);
-            vectors.Draw(CalculatePos(vectorC), CalculatePos(vectorAC), Color.red);
-            vectors.Draw(CalculatePos(vectorB), CalculatePos(vectorAB), Color.red);
-            vectors.Draw(CalculatePos(vectorBC), CalculatePos(vectorABC), Color.red);
             
-            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorC), Color.blue);
-            vectors.Draw(CalculatePos(vectorA), CalculatePos(vectorAC), Color.blue);
-            vectors.Draw(CalculatePos(vectorB), CalculatePos(vectorBC), Color.blue);
-            vectors.Draw(CalculatePos(vectorAB), CalculatePos(vectorABC), Color.blue);
+            DrawCube(A);
+            DrawCube(B);
+            DrawCube(C);
             
-            vectors.Draw(CalculatePos(vector0), CalculatePos(vectorB), Color.green);
-            vectors.Draw(CalculatePos(vectorA), CalculatePos(vectorAB), Color.green);
-            vectors.Draw(CalculatePos(vectorC), CalculatePos(vectorBC), Color.green);
-            vectors.Draw(CalculatePos(vectorAC), CalculatePos(vectorABC), Color.green);
+            
+            void DrawCube(Matrix4x4 mat)
+            {
+                vectors.Draw(CalculatePos(vector0, mat), CalculatePos(vectorA, mat), Color.red);
+                vectors.Draw(CalculatePos(vectorC, mat), CalculatePos(vectorAC, mat), Color.red);
+                vectors.Draw(CalculatePos(vectorB, mat), CalculatePos(vectorAB, mat), Color.red);
+                vectors.Draw(CalculatePos(vectorBC, mat), CalculatePos(vectorABC, mat), Color.red);
+                
+                vectors.Draw(CalculatePos(vector0, mat), CalculatePos(vectorC, mat), Color.blue);
+                vectors.Draw(CalculatePos(vectorA, mat), CalculatePos(vectorAC, mat), Color.blue);
+                vectors.Draw(CalculatePos(vectorB, mat), CalculatePos(vectorBC, mat), Color.blue);
+                vectors.Draw(CalculatePos(vectorAB, mat), CalculatePos(vectorABC, mat), Color.blue);
+                
+                vectors.Draw(CalculatePos(vector0, mat), CalculatePos(vectorB, mat), Color.green);
+                vectors.Draw(CalculatePos(vectorA, mat), CalculatePos(vectorAB, mat), Color.green);
+                vectors.Draw(CalculatePos(vectorC, mat), CalculatePos(vectorBC, mat), Color.green);
+                vectors.Draw(CalculatePos(vectorAC, mat), CalculatePos(vectorABC, mat), Color.green);
+            }
         }
 
-        Vector4 CalculatePos(Vector4 vec)
+        Vector4 CalculatePos(Vector4 vec, Matrix4x4 mat)
         {
-            return C*(vec);
+            return mat*(vec);
         }
 
         Vector3 QuatMultVector3(Quaternion quat, Vector3 vec)
@@ -148,6 +176,103 @@ public class Example : MonoBehaviour {
             return new Vector3(temp.x, temp.y, temp.z);
         }
 
+        Quaternion weeewooo(Matrix4x4 matrix1, Matrix4x4 matrix2)
+        {
+            var planeNormal = Vector3.zero;
+            for (int i = 0; i < 3; i++)
+            {
+                planeNormal += Math.CrossProduct(Math.Normalize(Math.getColumnFromMatrix(matrix1, i)),
+                    Math.Normalize(Math.getColumnFromMatrix(matrix2, i)));
+            }
+            if (planeNormal == Vector3.zero)
+                return Quaternion.identity;
+
+            planeNormal = Math.Normalize(planeNormal);
+            
+            float xDiff = Math.DotProduct(planeNormal, Math.getColumnFromMatrix(matrix1, 0));
+            float yDiff = Math.DotProduct(planeNormal, Math.getColumnFromMatrix(matrix1, 1));
+            float zDiff = Math.DotProduct(planeNormal, Math.getColumnFromMatrix(matrix1, 2));
+            
+            Vector3 matrix2proj;
+            Vector3 matrix1proj;
+            if (xDiff <= yDiff && xDiff <= zDiff)
+            {
+                matrix2proj = Proj(0, matrix2);
+
+                matrix1proj = Proj(0, matrix1);
+            }
+            else if (yDiff <= xDiff && yDiff <= zDiff)
+            {
+                matrix2proj = Proj(1, matrix2);
+
+                matrix1proj = Proj(1, matrix1);
+            }
+            else if (zDiff <= xDiff && zDiff <= yDiff)
+            {
+                matrix2proj = Proj(2, matrix2);
+
+                matrix1proj = Proj(2, matrix1);
+            }
+            else
+            {
+                matrix2proj = Vector3.one;
+                matrix1proj = Vector3.zero;
+                Debug.Log("Grått gråt");
+            }
+            
+            
+            
+            //Hjälp
+            float cosAngle = Math.DotProduct(Math.Normalize(matrix2proj), Math.Normalize(matrix1proj));
+            
+            while (cosAngle<-1)
+            {
+                cosAngle += 1;
+            }
+
+            while (cosAngle>1)
+            {
+                cosAngle -= 1;
+            }
+            
+            float angle = Mathf.Acos(cosAngle)/2;
+            
+            return  new Quaternion(Mathf.Sin(angle ) * planeNormal.x,
+                Mathf.Sin(angle) * planeNormal.y,Mathf.Sin(angle) * planeNormal.z, Mathf.Cos(angle));
+            
+            
+            //Slut på hjälp
+            
+            
+            
+            Vector3 Proj(int i, Matrix4x4 matrix4X4)
+            {
+                return Math.getColumnFromMatrix(matrix4X4, i) -
+                       Math.DotProduct(Math.getColumnFromMatrix(matrix4X4, i) , planeNormal) * planeNormal;
+            }
+        }
+
+
+
+
+
+
+        Quaternion GetInterpolatedQuat(Quaternion a, Quaternion b)
+        {
+            var aInverse = new Quaternion(a.x, a.y, a.z, -a.w);
+            Quaternion c = b * aInverse;
+            var oldAngle = Mathf.Acos(c.w);
+            var newAngle = oldAngle * Time;
+            var rotationAxis = new Vector3(c.x / Mathf.Sin(oldAngle), c.y / Mathf.Sin(oldAngle),
+                c.z / Mathf.Sin(oldAngle));
+            var newRotationAxis = rotationAxis * newAngle;
+            var interpolatedQuat = new Quaternion(newRotationAxis.x, newRotationAxis.y, newRotationAxis.z, newAngle);
+            return interpolatedQuat * a;
+        }
+        
+        
+        
+        
         Quaternion Rotationweee(float t)
         {
             var planeNormal = Vector3.zero;
@@ -204,7 +329,7 @@ public class Example : MonoBehaviour {
                 cosAngle -= 1;
             }
             
-            float angle = Mathf.Acos(cosAngle)/2;
+            float angle = Mathf.Acos(cosAngle)/2*t;
             
             Quaternion aaxe =  new Quaternion(Mathf.Sin(angle ) * planeNormal.x,
                 Mathf.Sin(angle) * planeNormal.y,Mathf.Sin(angle) * planeNormal.z, Mathf.Cos(angle));
@@ -218,18 +343,36 @@ public class Example : MonoBehaviour {
 
             Debug.Log($"Q:{q}");
             return aaxe;
-            
-            
-            
-            
+        
             Vector3 Proj(int i, Matrix4x4 matrix4X4)
             {
-                return Math.Normalize(Math.getColumnFromMatrix(matrix4X4, i) -
-                    Math.DotProduct(Math.getColumnFromMatrix(matrix4X4, i) , planeNormal) * planeNormal);
+                return Math.getColumnFromMatrix(matrix4X4, i) -
+                       Math.DotProduct(Math.getColumnFromMatrix(matrix4X4, i) , planeNormal) * planeNormal;
             }
         }
     }
+    
+    
+    
+    public Quaternion QuaternionFromMatrix(Matrix4x4 m){
+        Quaternion q = new Quaternion();
+        q.w = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] + m[1,1] + m[2,2] ) ) / 2; 
+        q.x = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] - m[1,1] - m[2,2] ) ) / 2; 
+        q.y = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] + m[1,1] - m[2,2] ) ) / 2; 
+        q.z = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] - m[1,1] + m[2,2] ) ) / 2; 
+        q.x *= Mathf.Sign( q.x * ( m[2,1] - m[1,2] ) );
+        q.y *= Mathf.Sign( q.y * ( m[0,2] - m[2,0] ) );
+        q.z *= Mathf.Sign( q.z * ( m[1,0] - m[0,1] ) );
+        return q;
+    }
+    
 }
+
+
+
+
+
+
 
 [CustomEditor(typeof(Example))]
 public class DemoEditor : Editor
--- Assets/Math.cs
+++ Assets/Math.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -42,5 +44,59 @@ public static class Math
         mat[2, i] = vec.z;
         
         return mat;
+    }
+
+    public static Matrix4x4 RotationMatrixFromQuaternion(Quaternion q)
+    {
+        var x = q.x;
+        var y = q.y;
+        var z = q.z;
+        var w = q.w;
+        var c1 = new Vector4(1-2*y*y-2*z*z,2*x*y+2*w*z,2*x*z-2*w*y,0);
+        var c2 = new Vector4(2*x*y-2*w*z,1-2*x*x-2*z*z,2*y*z+2*w*x, 0);
+        var c3 = new Vector4(2*x*z+2*w*y,2*y*z-2*w*x,1-2*x*x-2*y*y,0);
+        var c4 = new Vector4(0,0,0,1);
+
+        return new Matrix4x4(c1,c2,c3,c4);
+    }
+
+    public static Quaternion QuatFromMatrix(Matrix4x4 matrix4X4)
+    {
+        Vector3 x = new Vector3(matrix4X4.m00, matrix4X4.m10, matrix4X4.m20).normalized;
+        Vector3 y = new Vector3(matrix4X4.m01, matrix4X4.m11, matrix4X4.m21).normalized;
+        Vector3 z = new Vector3(matrix4X4.m02, matrix4X4.m12, matrix4X4.m22).normalized;
+        Quaternion quat = new Quaternion();
+        quat.w = Mathf.Sqrt(Mathf.Max(0, 1 + x.x + y.y + z.z))/2;
+        quat.x = Mathf.Sqrt(Mathf.Max(0, 1 + x.x - y.y - z.z))/2;
+        quat.y = Mathf.Sqrt(Mathf.Max(0, 1 - x.x + y.y - z.z))/2;
+        quat.z = Mathf.Sqrt(Mathf.Max(0, 1 - x.x - y.y + z.z))/2;
+        quat.x *= Mathf.Sign(quat.x * (y.z - z.y));
+        quat.y *= Mathf.Sign(quat.y * (z.x - x.z));
+        quat.z *= Mathf.Sign(quat.z * (y.z - z.y));
+        return quat;
+    }
+
+    public static Matrix4x4 CalculateRotationMatrix(Matrix4x4 A, Matrix4x4 B, float time)
+    {
+        Quaternion q1 = QuatFromMatrix(A);
+        Quaternion q2 = QuatFromMatrix(B);
+        q1.w = -q1.w;
+        
+        q2.w = -q2.w;
+        Quaternion q3 = q2 * q1;
+        
+
+        q1.w = -q1.w;
+        float alpha = Mathf.Acos(q3.w);
+        float newAlpha = alpha * time;
+        q3.w = Mathf.Cos(newAlpha);
+        float mult = Mathf.Sin(newAlpha) / Mathf.Sin(alpha);
+
+        q3.x *= mult;
+        q3.y *= mult;
+        q3.z *= mult;
+        q3 *= q1;
+
+        return RotationMatrixFromQuaternion(q3);
     }
 }
